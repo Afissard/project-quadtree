fc033401857ae1344972e1c9c3caaec64df07a30
Afissard <83972828+Afissard@users.noreply.github.com>
2023-10-28 16:27:42 +0200

    bonne idée mais stack overflow

--- quadtree/make.go
+++ quadtree/make.go
@@ -8,10 +8,78 @@ func MakeFromArray(floorContent [][]int) (q Quadtree) {
 		En sortie : un quadtree qui stocke la map
 
 		Idée algo (user de fonctions récursives):
-		1. déterminer le nombre de node quadtree au bout de l'arbre -> détermine le nombre total "n" de nodes/couches.
-		2. créer des nodes pour stocker les valeurs de la map associé aux coordonnées (couche n).
-		3. créer des nodes pour rattacher les node précédemment créés (couches n-couches).
+		1. déterminer le nombre de nodes quadtree à lié -> nombre total "n" de nodes pour cette couche.
+		3. créer des nodes pour rattacher les node précédemment créés (ou aux données de la map à itération n°1).
 		4. si couche 0 atteint (1 seule node à attacher) alors attacher la node restante à la racine.
 	*/
-	return
+	// calcul le nombre de nodes (en supposant que tout len(floorContent[x] = len(floorContent[0]) -> map rectangulaire)
+	var nbrNodes int = (len(floorContent) * len(floorContent[0])) / 4
+
+	if nbrNodes > 1 {
+		// création des node pour la couche "feuille"
+		nodesList := [][]node{}
+		for y := 0; y < len(floorContent); y++ {
+			nodesLine := []node{}
+			for x := 0; x < len(floorContent[y]); x++ {
+				currentNode := node{
+					topLeftX: x,
+					topLeftY: y,
+					width:    1,
+					height:   1,
+					content:  floorContent[y][x],
+				}
+				nodesLine = append(nodesLine, currentNode)
+			}
+			nodesList = append(nodesList, nodesLine)
+		}
+		q = createNodesLayer(nodesList)
+	} else { // si une seule node possible
+		nodeAlone := node{
+			topLeftX: 0,
+			topLeftY: 0,
+			width:    1,
+			height:   1,
+			content:  floorContent[0][0],
+		}
+		q.width = nodeAlone.width
+		q.height = nodeAlone.height
+		q.root = &nodeAlone
+	}
+	return q
+}
+
+func createNodesLayer(nodesList [][]node) (q Quadtree) {
+	// calcul le nombre de nodes (en supposant que tout len(floorContent[x] = len(floorContent[0]) -> map rectangulaire)
+	var nbrNodes int = (len(nodesList) * len(nodesList[0])) / 4
+
+	if nbrNodes > 1 {
+		// création des node pour cette couche
+		for y := 0; y < len(nodesList); y += 2 { // +2 au lieu de +1 car une node couvre 4 nodes
+			for x := 0; x < len(nodesList[y]); x += 2 {
+				currentNode := node{
+					topLeftX: x,
+					topLeftY: y,
+					width:    nodesList[y][x].width * 2,
+					height:   nodesList[y][x].height * 2,
+				}
+				// lie les nodes, en faisant attention au potentiel nodes inexistante
+				currentNode.topLeftNode = &nodesList[y][x]
+				if x+1 > len(nodesList[y]) {
+					currentNode.topRightNode = &nodesList[y][x+1]
+				}
+				if y+1 > len(nodesList) {
+					currentNode.bottomLeftNode = &nodesList[y+1][x]
+					if x+1 > len(nodesList[y]) {
+						currentNode.bottomRightNode = &nodesList[y+1][x+1]
+					}
+				}
+			}
+		}
+		q = createNodesLayer(nodesList)
+	} else { // si une seule node possible
+		q.width = nodesList[0][0].width
+		q.height = nodesList[0][0].height
+		q.root = &nodesList[0][0]
+	}
+	return q
 }
---
+++ quadtree/make_test.go
@@ -0,0 +1,20 @@
+package quadtree
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_MakeFromArray(t *testing.T) {
+	mapContent := [][]int{
+		{1, 1, 3, 4},
+		{1, 1, 4, 3},
+		{0, 0, 2, 2},
+		{0, 0, 2, 2},
+	}
+	q := MakeFromArray(mapContent)
+	if q.root == nil {
+		t.Fatal("floorContent est vide")
+	}
+	fmt.Println(q)
+}
