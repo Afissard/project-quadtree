45f3abdcd7ee0bbb7ae6066092cf2cccc8e5342d
Afissard <83972828+Afissard@users.noreply.github.com>
2023-11-08 13:23:50 +0100

    start working on corridor (BSP)

---
+++ bsp_dungeon/corridor.go
@@ -0,0 +1,10 @@
+package bsp_dungeon
+
+import "fmt"
+
+/*
+Fonction pour créer et remplire le contenu des couloirs
+*/
+func (level BSP_tree) createCorridor() {
+	fmt.Println(level) // juste pour eviter erreur de compilation
+}
--- bsp_dungeon/create.go
+++ bsp_dungeon/create.go
@@ -75,27 +75,3 @@ func createNode(alea *rand.Rand, parent *node, nbrDivLeft, width, height, topLef
 	return currentNode, roomList
 }
 
-/*
-Fonction pour créer des salle ou des couloir
-- ajoute des salle au node feuille
-- créé un couloir entre les nodes enfant (les couloir seront générer plus tard dans le programme)
-- le contenu des salle/couloir sera déterminé plus tard aussi
-*/
-func createRoom(currentNode *node, alea *rand.Rand) (newRoom *room) {
-	newRoom = &room{}
-
-	isRoom := true
-	if len(currentNode.children) > 0 { // couloir
-		newRoom.isRoom = false
-	} else {
-		newRoom = &room{ // salle
-			isRoom:       isRoom,
-			width:        alea.Intn(int(float32(currentNode.width)*0.70-float32(currentNode.width)*0.30)) + int(float32(currentNode.width)*0.30),
-			height:       alea.Intn(int(float32(currentNode.height)*0.70-float32(currentNode.height)*0.30)) + int(float32(currentNode.height)*0.30),
-			floorContent: 0,
-		}
-		newRoom.topLeftX = alea.Intn(currentNode.width-newRoom.width) + currentNode.topLeftX
-		newRoom.topLeftY = alea.Intn(currentNode.height-newRoom.height) + currentNode.topLeftY
-	}
-	return newRoom
-}
