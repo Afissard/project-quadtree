47730798e3e69cd0c986abf5d776dc38901f3c8f
Afissard <83972828+Afissard@users.noreply.github.com>
2023-11-09 07:46:24 +0100

    need to use 2D array for room

--- bsp_dungeon/corridor.go
+++ bsp_dungeon/corridor.go
@@ -1,7 +1,5 @@
 package bsp_dungeon
 
-import "fmt"
-
 /*
 Fonction pour créer et remplir le contenu des couloirs
 Algo :
@@ -25,28 +23,30 @@ func createCorridor(currentNode *node) (content *room, roomList []*room) {
 			isRoom:       false,
 			floorContent: 2,
 		}
-
-		//TODO: centré/positionné aléatoirement le couloir + taille ~fixe
-		// possibilité de virage dans les couloir si trop large -> Pythagore ?
+		/*
+			TODO: centré/positionné aléatoirement le couloir + taille ~fixe
+				possibilité de virage dans les couloir si trop large -> Pythagore ?
+			TODO: ne relié que les salles, donc choisir une des salle plus bas
+				dans l'arbre si isRoom = False
+		*/
 		if roomA.topLeftX < roomB.topLeftX {
 			corridor.topLeftX = roomA.topLeftX
-			corridor.width = roomB.topLeftX - roomA.topLeftX
+			corridor.width = roomB.topLeftX - roomA.topLeftX + 1
 		} else {
 			corridor.topLeftX = roomB.topLeftX
-			corridor.width = roomA.topLeftX - roomB.topLeftX
+			corridor.width = roomA.topLeftX - roomB.topLeftX + 1
 		}
 
 		if roomA.topLeftY < roomB.topLeftY {
 			corridor.topLeftY = roomA.topLeftY
-			corridor.height = roomB.topLeftY - roomA.topLeftY
+			corridor.height = roomB.topLeftY - roomA.topLeftY + 1
 		} else {
 			corridor.topLeftY = roomB.topLeftY
-			corridor.height = roomA.topLeftY - roomB.topLeftY
+			corridor.height = roomA.topLeftY - roomB.topLeftY + 1
 		}
 
 		currentNode.content = &corridor
 		roomList = append(roomList, currentNode.content)
-		fmt.Println(roomList)
 		return &corridor, roomList
 	} else { // si au bout de l'arbre
 		return currentNode.content, roomList
