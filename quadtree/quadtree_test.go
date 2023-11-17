3e1eb414bb7409a66bfa78552e67335798c9f3e9
Afissard <83972828+Afissard@users.noreply.github.com>
2023-11-17 19:47:25 +0100

    add test

--- quadtree/quadtree_test.go
+++ quadtree/quadtree_test.go
@@ -6,11 +6,42 @@ import (
 	"testing"
 )
 
+// Test la validité de MakeFromArray et GetContent
+func Test_GetContent_n0(t *testing.T) {
+	fmt.Println("\nTest GetContent n°0:")
+
+	// initialisation :
+	mapContent := [][]int{}
+	contentHolder := [][]int{}
+	maxY, maxX := 64, 64
+	for y := 0; y <= maxY; y++ {
+		lineMap, lineHolder := []int{}, []int{}
+		for x := 0; x <= maxX; x++ {
+			lineMap = append(lineMap, 4)
+			lineHolder = append(lineHolder, -1)
+		}
+		mapContent = append(mapContent, lineMap)
+		contentHolder = append(contentHolder, lineHolder)
+	}
+
+	// fonctions :
+	q := MakeFromArray(mapContent)
+	q.root.content = optimize(q.root)
+	q.GetContent(0, 0, contentHolder)
+
+	// tests :
+	if !reflect.DeepEqual(mapContent, contentHolder) {
+		t.Fatalf("\nmapContent : %d,\ndifférent de contentHolder : %d", mapContent, contentHolder)
+	} else {
+		fmt.Printf("\nok : mapContent : %d == contentHolder : %d\n", mapContent, contentHolder)
+	}
+}
+
 func Test_GetContent_n1(t *testing.T) {
 	/*
 		Test la validité de MakeFromArray et GetContent
 	*/
-	fmt.Println("\nTest GetContent (n°1):")
+	fmt.Println("\nTest GetContent n°1:")
 	mapContent := [][]int{
 		{1, 1, 3, 4},
 		{1, 1, 4, 3},
@@ -36,7 +67,7 @@ func Test_GetContent_n2(t *testing.T) {
 	/*
 		Test la validité de MakeFromArray et GetContent
 	*/
-	fmt.Println("\nTest GetContent (n°2):")
+	fmt.Println("\nTest GetContent n°2:")
 	mapContent := [][]int{
 		{1, 1, 3, 4, 1, 1, 3, 4},
 		{1, 1, 4, 3, 1, 1, 4, 3},
@@ -70,7 +101,7 @@ func Test_GetContent_n1_optimized(t *testing.T) {
 	/*
 		Test la validité de MakeFromArray et GetContent
 	*/
-	fmt.Println("\nTest GetContent (n°1 optimized):")
+	fmt.Println("\nTest GetContent n°1 optimized:")
 	mapContent := [][]int{
 		{1, 1, 3, 4},
 		{1, 1, 4, 3},
@@ -102,7 +133,7 @@ func Test_GetContent_n2_optimized(t *testing.T) {
 	/*
 		Test la validité de MakeFromArray et GetContent
 	*/
-	fmt.Println("\nTest GetContent (n°2 optimized):")
+	fmt.Println("\nTest GetContent n°2 optimized:")
 	mapContent := [][]int{
 		{1, 1, 3, 4, 1, 1, 3, 4},
 		{1, 1, 4, 3, 1, 1, 4, 3},
@@ -139,7 +170,7 @@ func Test_GetContent_n3_optimized(t *testing.T) {
 	/*
 		Test la validité de MakeFromArray et GetContent
 	*/
-	fmt.Println("\nTest GetContent (n°3 optimized):")
+	fmt.Println("\nTest GetContent n°3 optimized:")
 	mapContent := [][]int{
 		{0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0},
 		{1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 2, 2, 1, 2, 1, 2, 1, 2, 2, 2, 1},
