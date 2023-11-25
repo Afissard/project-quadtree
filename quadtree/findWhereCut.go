7cbee972be20a99aacacc632482b9304ff8c11e8
nakaeuna <lcortet@icloud.com>
2023-11-25 15:38:48 +0100

    1

---
+++ quadtree/findWhereCut.go
@@ -0,0 +1,12 @@
+package quadtree
+
+func findWhereCut(co de la où on veut ou on veut acceder, arbre avec toutes les nodes)
+
+
+	return pointeur de la node où on trouve les co
+
+cas: où on peut mettre -> branche de libre : ajouter contenu dans la branche
+
+cas: où non -> tout reconstruire à partir de cet endroit
+
+-> on vient la modifier ensuite
