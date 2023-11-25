ce7f0e7698aa00f1990654d0ed4999fd7fbc9e5f
Afissard <83972828+Afissard@users.noreply.github.com>
2023-11-25 16:57:54 +0100

    cleaning stuff

--- quadtree/appendToQuadtree.go
+++ quadtree/appendToQuadtree.go
@@ -1,6 +1,6 @@
 package quadtree
 
-func (q Quadtree) AppendToQuadtree(nodeToMod, newChunkTree *node) {
+func (q Quadtree) AppendToQuadtree(nodeToMod, newNode *node) {
 	/*
 		Algo :
 		1. détermine où couper par rapport à l'input
@@ -8,21 +8,21 @@ func (q Quadtree) AppendToQuadtree(nodeToMod, newChunkTree *node) {
 			- cas où il faut créer une node supérieur
 		2. reconstruit l'arbre (attention aux dimensions des node au dessus de la découpe)
 	*/
-	if newChunkTree.topLeftX < nodeToMod.topLeftX+nodeToMod.width/2 {
-		if newChunkTree.topLeftY < nodeToMod.topLeftY+nodeToMod.height/2 {
+	if newNode.topLeftX < nodeToMod.topLeftX+nodeToMod.width/2 {
+		if newNode.topLeftY < nodeToMod.topLeftY+nodeToMod.height/2 {
 			// tl
-			nodeToMod.topLeftNode = newChunkTree
+			nodeToMod.topLeftNode = newNode
 		} else {
 			// bl
-			nodeToMod.bottomLeftNode = newChunkTree
+			nodeToMod.bottomLeftNode = newNode
 		}
-	} else if newChunkTree.topLeftX <= nodeToMod.topLeftX+nodeToMod.width/2 {
-		if newChunkTree.topLeftY < nodeToMod.topLeftY+nodeToMod.height/2 {
+	} else if newNode.topLeftX <= nodeToMod.topLeftX+nodeToMod.width/2 {
+		if newNode.topLeftY < nodeToMod.topLeftY+nodeToMod.height/2 {
 			// tr
-			nodeToMod.topRightNode = newChunkTree
+			nodeToMod.topRightNode = newNode
 		} else {
 			// br
-			nodeToMod.bottomRightNode = newChunkTree
+			nodeToMod.bottomRightNode = newNode
 		}
 	} else {
 		// créer la nouvelle node "root"
@@ -30,13 +30,13 @@ func (q Quadtree) AppendToQuadtree(nodeToMod, newChunkTree *node) {
 		rootNode := node{
 			topLeftX: 0,
 			topLeftY: 0,
-			width:    oldRootNode.width + newChunkTree.width,
-			height:   oldRootNode.height + newChunkTree.height,
+			width:    oldRootNode.width + newNode.width,
+			height:   oldRootNode.height + newNode.height,
 			content:  -1,
 		}
 		q.AppendToQuadtree(&rootNode, oldRootNode)
 		q.root = &rootNode
 		// réessaye de rajouté le chunk
-		q.AppendToQuadtree(q.root, newChunkTree)
+		q.AppendToQuadtree(q.root, newNode)
 	}
 }
