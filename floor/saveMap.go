86d349e5457d0d3274be1e731bc3ed1fc92d2f75
Afissard <83972828+Afissard@users.noreply.github.com>
2023-12-05 22:05:33 +0100

    found origin of the bug : fullContent is empty

--- floor/saveMap.go
+++ floor/saveMap.go
@@ -9,21 +9,27 @@ import (
 
 func check(err error) {
 	if err != nil {
-		fmt.Println(err.Error())
+		panic(err)
 	}
 }
 
 func (f *Floor) SaveMap(fileName string) (err error) {
+	// Récupération de la map actuelle
+	f.quadtreeContent.GetContent(0, 0, f.fullContent) // BUG : vide
+	fmt.Println("fullContent : ", f.fullContent)
+
+	// Création du fichier de sauvegarde
 	_, errFileExist := os.Stat("../floor-files/" + fileName) // test l'existence du fichier save
 	if errFileExist == nil {
-		err := os.Remove("../floor-files/" + fileName) // Ouvre le fichier save pour le réécrire
+		err := os.Remove("../floor-files/" + fileName) // si il existe : le supprime pour le recréer (évite bug de réécriture)
 		check(err)
 	}
 	mapFile, err := os.Create("../floor-files/" + fileName) // Créer le fichier save
 	check(err)
 	defer mapFile.Close()
-	w := bufio.NewWriter(mapFile)
 
+	// Écriture du fichier de sauvegarde
+	w := bufio.NewWriter(mapFile)
 	bytesWritten := 0
 	for y := 0; y < len(f.fullContent); y++ {
 		//TODO: add modification to convert mapContent value to its equivalent in ASCII
@@ -35,7 +41,7 @@ func (f *Floor) SaveMap(fileName string) (err error) {
 		err := fmt.Errorf(fmt.Sprintf("Error : %d bytes écrits", bytesWritten))
 		check(err)
 	}
-	fmt.Printf("wrote %d bytes\n", bytesWritten)
+	// fmt.Printf("wrote %d bytes\n", bytesWritten)
 	err = w.Flush()
 	check(err)
 	return err
